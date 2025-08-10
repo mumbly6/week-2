bede61ada69a94656fc35b2be69c41f7e9b7b465
mumbly6 <mumblykilonzo@gmail.com>
2025-08-10 17:40:44 +0300

    Add files via upload

---
+++ week 2.py
@@ -0,0 +1,23 @@
+my_list = []
+print(f"{my_list}")
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(f"{my_list}")
+my_list.insert(1, 15)
+print(f"{my_list}")
+my_list.extend([50, 60, 70])
+print(f" {my_list}")
+my_list.pop()
+print(f"{my_list}")
+my_list.sort()
+print(f" {my_list}")
+index_of_30 = my_list.index(30)
+print(f" {index_of_30}")+
+
+print(f"\nFinal list: {my_list}")
+
+
+
+
